2b2db1bf5ba0febf1d8fe20b3d80562af2073989
TurboKach <incognito@turbokach.me>
2021-11-22 11:44:44 +0300

    test update

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -1,5 +1,4 @@
 import os
-import re
 
 from .conftest import root_dir
 
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,9 +3,9 @@ from api_yamdb import settings
 
 class TestSettings:
 
-    def test_dockerfile(self):
+    def test_settings(self):
 
         assert not settings.DEBUG, 'Проверьте, что DEBUG в настройках Django выключен'
         assert settings.DATABASES['default']['ENGINE'] == 'django.db.backends.postgresql', (
-            'Проверьте, что используете базу данных PostgreSql'
+            'Проверьте, что используете базу данных postgresql'
         )
